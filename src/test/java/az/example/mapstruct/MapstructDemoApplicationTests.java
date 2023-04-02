86939d1a71864b33aeb9e50f3c659a95e842f95d
elxan <elxan@mina.az>
2023-04-02 12:49:19 +0400

    commented whole test package

--- src/test/java/az/example/mapstruct/MapstructDemoApplicationTests.java
+++ src/test/java/az/example/mapstruct/MapstructDemoApplicationTests.java
@@ -1,13 +1,13 @@
-package az.example.mapstruct;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MapstructDemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package az.example.mapstruct;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MapstructDemoApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
